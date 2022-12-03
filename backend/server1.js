be0ad28d6a4e0e01408e329f107a4b66d303e0f4
Nicholas Cai <cainicholas91@gmail.com>
2022-12-03 05:43:23 -0500

    new api of goals but this is for play testing

--- backend/server1.js
+++ backend/server1.js
@@ -1,5 +1,27 @@
+
+
 const express = require('express')
 const dotenv = require('dotenv').config();
 const port = 5001
+const {errorHandler} = require('./middleware/errorMiddleware') 
+
+const colors = require('colors')
+
+const connectDB = require('./config/db')
+connectDB()
+
+
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+app.use('/api/goals',require('./routes/Routes'))
+
+
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
