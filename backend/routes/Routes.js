69a74b4916f8cf66abc4500e2babf19b1a1570f4
Nicholas Cai <cainicholas91@gmail.com>
2022-12-03 16:54:54 -0500

    added testing

--- backend/routes/Routes.js
+++ backend/routes/Routes.js
@@ -3,11 +3,8 @@ const router = express.Router()
 
 const {getGoal,setGoal,updateGoal,deleteGoal} = require('../controller/Controller')
 
-
-
 //get //post
 router.route('/').get(getGoal).post(setGoal)
-
 
 //put //delete
 router.route('/:id').put(updateGoal).delete(deleteGoal)
--- backend/server1.js
+++ backend/server1.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express')
 const dotenv = require('dotenv').config();
 const port = 5001
@@ -8,20 +6,20 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 const colors = require('colors')
 
 const connectDB = require('./config/db')
-connectDB()
-
 
 
 const app = express()
 
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
 app.use(express.json())
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({extended : false})) 
 
 app.use('/api/goals',require('./routes/Routes'))
-
-
+//uses route --> controller --> model
 
 app.use(errorHandler)
+connectDB()
+//error and db connections are directly run in the server 1
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
 
